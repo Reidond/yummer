b4f919d129185cc5135b83b0d16cd165e9d91bdd
Reidond <reidond@gmail.com>
2019-12-25 19:55:24 +0200

    Move to class components

--- src/App.vue
+++ src/App.vue
@@ -41,17 +41,14 @@
 
 <script lang="ts">
 import Vue from "vue";
+import Component from "vue-class-component";
 import HelloWorld from "./components/HelloWorld.vue";
 
-export default Vue.extend({
+@Component({
   name: "App",
-
   components: {
     HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-});
+  }
+})
+export default class App extends Vue {}
 </script>
